b7037b61adc5d9ef8cd5a9f3bf60c59c872009d6
ripper <453942056@qq.com>
2019-05-11 16:33:29 +0800

    no commit info

--- Debugo/Plugin/DGTouchMonitor/DGTouchFingerView.m
+++ Debugo/Plugin/DGTouchMonitor/DGTouchFingerView.m
@@ -11,7 +11,7 @@
 #import "DGTouchFingerView.h"
 
 CGFloat const DGDefaultMaxFingerRadius = 22.0;
-CGFloat const DGDefaultForceTouchScale = 1.8;
+CGFloat const DGDefaultForceTouchScale = 1.5;
 
 @interface DGTouchFingerView ()
 
